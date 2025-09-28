3ea1f5abc1bfbd4334a72ed68c727397434fa732
Willy Douhard <willy.douhard@gmail.com>
2025-09-28 10:56:23 +0200

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,18 +5,18 @@ set -e
 make redis-on-docker
 make postgres-on-docker
 
-yarn
+yarn install
 
-npx nx database:reset twenty-server
+./nx database:reset twenty-server
 
 # Ensure log directory exists
 mkdir -p "$TWILL_ENTRYPOINT_LOG_DIR"
 
-nohup npx nx start twenty-server > "$TWILL_ENTRYPOINT_LOG_DIR/server.log" 2>&1 &
-nohup npx nx worker twenty-server > "$TWILL_ENTRYPOINT_LOG_DIR/worker.log" 2>&1 &
-nohup npx nx start twenty-front > "$TWILL_ENTRYPOINT_LOG_DIR/front.log" 2>&1 &
+nohup ./nx start twenty-server > "$TWILL_ENTRYPOINT_LOG_DIR/server.log" 2>&1 &
+nohup ./nx worker twenty-server > "$TWILL_ENTRYPOINT_LOG_DIR/worker.log" 2>&1 &
+nohup ./nx start twenty-front > "$TWILL_ENTRYPOINT_LOG_DIR/front.log" 2>&1 &
 
-echo "Frontend started on http://localhost:3001"
-echo "GraphQL API started on http://localhost:3000/graphql"
-echo "REST API started on http://localhost:3000/rest"
+echo "Frontend starting on http://localhost:3001"
+echo "GraphQL API starting on http://localhost:3000/graphql"
+echo "REST API starting on http://localhost:3000/rest"
 
