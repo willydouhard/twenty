c934f2daf70654318d902f3868fea8986974c241
Willy Douhard <willy.douhard@gmail.com>
2025-09-29 12:26:02 +0200

    remove sequential build

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,19 +7,21 @@ make postgres-on-docker || echo "Postgres already running, continuing"
 
 yarn
 
-# Building sequentially to avoid exhausting memory
 
 echo "Resetting database"
 ./nx database:reset twenty-server > /dev/null
 
-echo "Building server"
-./nx build twenty-server
 
-echo "Typechecking server"
-./nx typecheck twenty-server
+# Building sequentially to avoid exhausting memory
 
-echo "Building UI"
-./nx build twenty-ui
+# echo "Building server"
+# ./nx build twenty-server
+
+# echo "Typechecking server"
+# ./nx typecheck twenty-server
+
+# echo "Building UI"
+# ./nx build twenty-ui
 
 # Ensure log directory exists
 mkdir -p "$TWILL_ENTRYPOINT_LOG_DIR"
